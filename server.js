ab388cd34712ff3587ba695c780bafdc9b35e923
EdenAlmighty <edengilady1@gmail.com>
2024-09-05 17:26:17 +0300

    Build v1.3 dark mode support

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ if (process.env.NODE_ENV === 'production') {
         origin: [
             'http://127.0.0.1:5174',
             'http://localhost:5174',
+            'http://localhost:5173',
+            'http://localhost:5173',
             'http://127.0.0.1:8080',
             'http://localhost:8080',
         ],
